1d3602497343a146c06400dd8fc19e3837f6fd64
Bhaskoro Muthohar <bhaskoro.jr@gmail.com>
2023-11-06 08:01:22 +0700

    new pages

--- pages/home.py
+++ pages/home.py
@@ -1,95 +1,62 @@
 import streamlit as st
-import duckdb
 import pandas as pd
 import plotly.express as px
-import plotly.figure_factory as ff
 
+# Load temperature data
+temperature_data_path = 'data/temperature_model - temperature_model.csv'
+temperature_data = pd.read_csv(temperature_data_path)
 
-# dummy data for the crop calendar
-crop_data = [
-    dict(Task="Crop A", Start='2023-01-01', Finish='2023-02-28', Resource='Planting'),
-    dict(Task="Crop B", Start='2023-03-01', Finish='2023-04-15', Resource='Growing'),
-    dict(Task="Crop C", Start='2023-04-16', Finish='2023-06-30', Resource='Harvesting'),
-]
+# Convert date to datetime
+temperature_data['date'] = pd.to_datetime(temperature_data['date'])
 
-def adjust_annotation_position(annotations, input, default_ay=-40):
-    same_day_events = [ann for ann in annotations if ann['x'] == input['Date'] and ann['y'] == input['Task Index']]
-    return default_ay - (len(same_day_events) * 20)
+# Create dropdown for subdistricts
+subdistricts = temperature_data['bps_kecamatan_nama'].unique()
+selected_subdistrict = st.selectbox('Select Subdistrict', options=subdistricts)
 
-# Dummy user inputs for specific events, including multiple events on the same date
-user_inputs = [
-    dict(Task="Crop A", Date='2023-02-15', Event='Banjir'),
-    dict(Task="Crop A", Date='2023-02-15', Event='Longsor'),
-    dict(Task="Crop B", Date='2023-03-20', Event='Pupuk Mahal Bosku'),
-    dict(Task="Crop C", Date='2023-05-15', Event='Cangkul cangkul yang dalam'),
-    dict(Task="Crop C", Date='2023-05-15', Event='Menunggu panen'),
-]
+# Filter data based on the selected subdistrict
+filtered_data = temperature_data[temperature_data['bps_kecamatan_nama'] == selected_subdistrict]
 
-selected_crops = st.multiselect('Select crops to display', options=[task['Task'] for task in crop_data], default=[task['Task'] for task in crop_data])
-
-filtered_crop_data = [task for task in crop_data if task['Task'] in selected_crops]
-
-fig = ff.create_gantt(filtered_crop_data, index_col='Resource', title='Crop Calendar', show_colorbar=True, group_tasks=True)
-
-annotations = []
-for input in user_inputs:
-    if input['Task'] in selected_crops:
-        task_index = [i for i, task in enumerate(filtered_crop_data) if task['Task'] == input['Task']][0]
-        ay_offset = adjust_annotation_position(annotations, {'Date': input['Date'], 'Task Index': task_index})
-        hover_text = f"{input['Event']} on {input['Date']}"
-        annotations.append(dict(
-            x=input['Date'],
-            y=task_index,
-            text=input['Event'],
-            hovertext=hover_text,
-            showarrow=True,
-            arrowhead=7,
-            ax=0,
-            ay=ay_offset
-        ))
-
-for task in filtered_crop_data:
-    task['hovertext'] = f"{task['Task']}<br>Start: {task['Start']}<br>Finish: {task['Finish']}"
-
-fig['layout']['annotations'] = annotations
-
-fig.update_layout(hovermode='closest')
+# Plotting
+fig = px.line(filtered_data, x='date', y='temperature', title=f'Temperature Over Time for {selected_subdistrict}')
+# Highlight nodes where temperature is extreme
+extreme_temps = filtered_data[filtered_data['is_temperature_extreme']]
+fig.add_scatter(x=extreme_temps['date'], y=extreme_temps['temperature'], mode='markers', name='Extreme Temperature', marker=dict(color='red', size=8))
 
 st.plotly_chart(fig, use_container_width=True)
 
-conn = duckdb.connect('db/duckdb_file.duckdb')
+# Optional: Display raw data
+if st.checkbox('Show raw data'):
+    st.write(filtered_data)
 
-query_date_range = "SELECT MIN(date) as min_date, MAX(date) as max_date FROM precipitation"
-date_range = conn.execute(query_date_range).fetchdf()
-min_date = pd.to_datetime(date_range['min_date'].iloc[0]).date()
-max_date = pd.to_datetime(date_range['max_date'].iloc[0]).date()
+# Load temperature data
+temperature_data_path = 'data/temperature_model - temperature_model.csv'
+temperature_data = pd.read_csv(temperature_data_path)
 
-st.markdown('**Precipitation Map - Java Island**')
+# Convert date to datetime
+temperature_data['date'] = pd.to_datetime(temperature_data['date'])
+
+# Determine the minimum and maximum dates for the slider
+min_date = temperature_data['date'].min().date()
+max_date = temperature_data['date'].max().date()
+
+st.markdown('**Temperature Map - Java Island**')
 selected_date = st.slider('Select date', min_value=min_date, max_value=max_date, value=min_date, format='YYYY-MM-DD')
 
-# Fetch data for the selected date
-query = f"""
-SELECT latitude, longitude, value, date
-FROM precipitation
-WHERE date = '{selected_date}'
-AND latitude BETWEEN -8.5 AND -6.0
-AND longitude BETWEEN 105.0 AND 114.0
-"""
-precipitation_data = conn.execute(query).fetchdf()
+# Filter data for the selected date
+selected_data = temperature_data[temperature_data['date'].dt.date == selected_date]
 
-# Close the database connection
-conn.close()
-
-if not precipitation_data.empty:
+if not selected_data.empty:
     fig_map = px.scatter_geo(
-        precipitation_data,
+        selected_data,
         lat='latitude',
         lon='longitude',
-        color='value',
-        hover_name='date',
-        projection='natural earth'
+        color='temperature',
+        hover_name='bps_kecamatan_nama',
+        projection='natural earth',
+        title=f"Temperature Data on {selected_date}"
     )
 
+    # Update map geos
     fig_map.update_geos(
         visible=True,
         lonaxis_range=[105, 114],  # Longitude range for Java
@@ -100,4 +67,4 @@ if not precipitation_data.empty:
 
     st.plotly_chart(fig_map, use_container_width=True)
 else:
-    st.write("No data available for the selected date.")
+    st.write("No temperature data available for the selected date.")
